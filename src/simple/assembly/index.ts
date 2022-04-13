aae63257d638904d7bb73da6d5f902db7fddae2f
Ege Güngördü <egegungorduu@hotmail.com>
2022-04-13 18:45:23 +0300

    assembly script practice task 2

--- src/simple/assembly/index.ts
+++ src/simple/assembly/index.ts
@@ -1,8 +1,9 @@
 import { storage, Context } from "near-sdk-as"
 
 // return the string 'hello world'
-export function helloWorld(names: Array<string>): string {
-  return names.map<string>(name => 'hello ' + name).join(` `)
+export function helloWorld(): string {
+  const predecessor = Context.predecessor
+  return 'hello ' + predecessor
 }
 
 // read the given key from account (contract) storage
